952b4dd2285696865c582b63a9802702fb682f74
roopkt <roopkt@gmail.com>
2017-04-05 18:41:36 -0400

    renamed file fixed compile errors

--- src/app/dynamic-form/containers/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/containers/dynamic-form/dynamic-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit, Output ,EventEmitter} from '@angular/core';
+
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
-import { DynamicFieldDirective } from '../../components/dynamic-field/dynamic-field.directive';
+import { FieldConfig } from '../../models/field-config.interface';
 @Component({
+    exportAs: 'dynamicForm',
     selector: 'dynamic-form',
     styleUrls: ['dynamic-form.component.scss'],
     template: `
@@ -13,7 +15,7 @@ import { DynamicFieldDirective } from '../../components/dynamic-field/dynamic-fi
 
     <ng-container 
         *ngFor="let field of config;" 
-        dynamicFieldDirective
+        dynamicField
         [config] = "field"
         [group] = "form">
     </ng-container>
@@ -23,11 +25,11 @@ import { DynamicFieldDirective } from '../../components/dynamic-field/dynamic-fi
 
 export class DynamicFormComponent implements OnInit {
     @Input()
-    config: any [] =[];
-    form:FormGroup;
+    config: any[] = [];
+    form: FormGroup;
     @Output()
-    submitted:EventEmitter<any> = new EventEmitter<any>();
-    constructor(private fb:FormBuilder) {
+    submitted: EventEmitter<any> = new EventEmitter<any>();
+    constructor(private fb: FormBuilder) {
     }
 
     ngOnInit() {
@@ -35,7 +37,7 @@ export class DynamicFormComponent implements OnInit {
     }
     createGroup() {
         const group = this.fb.group({});
-        this.config.forEach(control=>group.addControl(control.name, this.fb.control({})));
+        this.config.forEach(control => group.addControl(control.name, this.fb.control({})));
         return group;
     }
 }
